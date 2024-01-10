0ff6eecc107fcca1ec552159c7cf7f3c521943e3
Garin <61001058+Plusero@users.noreply.github.com>
2024-01-10 16:57:00 +0100

    1.10 end

--- case2.m
+++ case2.m
@@ -24,91 +24,16 @@ alpha_p=rho_p*c_pp*V_p/A_p/tau_p;
 alpha_w=rho_w*c_pw*V_w/A_w/tau_w;
 % Biot number calculations
 L_p=V_p/A_p;
-R_ext=1/(alpha_p-lambda_p*L_p);
 R_int=L_p/lambda_p;
+R_ext=1/alpha_p-R_int;
 Biot=R_int/R_ext;
 
 T_w_infy=170+273.15;%
 Ta=20+273.15;% 
 Q=(T_w_infy-Ta)*A_w*alpha_w;
 
-%simulations
+t_100=-log(7/15)*tau_w;
 
-% prepare workspace
-clear all
-close all
-clc
-format short
-
-% physical parameters
-tau_w = 100;
-tau_e = 50;
-Tw_inf = 150;
-Tw_inf_real = 100;
-Ta = 20;
-
-% numerical parameters
-dt = 60;  % time step (s)
-tend = 600;  % final time (s)
-t = 0:dt:tend;  % time vector (s)
-method = 'Heun';
-clip = 0;
-
-% analytical solution
-Tw_analyt = Tw_inf + (Ta - Tw_inf)*exp(-t/tau_w);
-
-% discretize time and temperatures
-imax = tend/dt;
-T = zeros(size(t,2), 2);
-
-% initialize temperatures
-T(1,1) = Ta;
-T(1,2) = Ta;
-
-% define ratio of time step and time constant
-h_w = dt/tau_w;
-h_e = dt/tau_e;
-
-% integrate differential equations numerically
-for i=1:imax
-    
-    switch method
-        case 'Euler'
-            % Euler's method
-            % water temperature (index 1)
-            T(i+1, 1) = (1 - h_w)*T(i, 1) + h_w*Tw_inf;
-            
-            % clip maximum water temperature to realistic value
-            if clip & T(i+1, 1) > Tw_inf_real
-                T(i+1, 1) = Tw_inf_real;
-                Tw_analyt(i+1) = Tw_inf_real;
-            end                
-    
-            % egg temperature (index 2)
-            T(i+1, 2) = (1 - h_e)*T(i, 2) + h_e*T(i, 1);
-            
-        case 'Heun'
-            % Heun's or trapezium method
-            % water temperature (index 1)
-            T(i+1, 1) = (1-0.5*h_w)/(1+0.5*h_w)*T(i, 1) + h_w/(1+0.5*h_w)*Tw_inf;
-            
-            % clip maximum water temperature to realistic value
-            if clip & T(i+1, 1) > Tw_inf_real
-                T(i+1, 1) = Tw_inf_real;
-                Tw_analyt(i+1) = Tw_inf_real;
-            end 
-            
-            T(i+1, 2) = (1-0.5*h_e)/(1+0.5*h_e)*T(i, 2)...
-                      + h_e/(1+0.5*h_e)*(T(i, 1) + T(i+1, 1))/2;
-    end
-end
-
-% plot temperature profile and error
-figure(1);
-plot(t, Tw_analyt, '-', t, T, 'o--');
-xlabel('time, t / s')
-ylabel('temperatures, T / °C')
-legend('analyt: water', 'num: water', 'num: egg', 'Location','east')
 
 
 
---
+++ case2_sim_7.m
@@ -0,0 +1,98 @@
+% prepare workspace
+clear all
+close all
+clc
+format short
+
+% physical parameters
+tau_w = 5500;
+tau_e = 400;
+Tw_inf = 170;
+Tw_inf_real = 100;
+Ta = 20;
+
+% numerical parameters
+dt = 60  % time step (s)
+tend = 600;  % final time (s)
+t = 0:dt:tend;  % time vector (s)
+
+% analytical solution
+Tw_analyt = Tw_inf + (Ta - Tw_inf)*exp(-t/tau_w);
+
+% discretize time and temperatures
+imax = tend/dt;
+T_euler = zeros(size(t,2), 2);
+T_tra = zeros(size(t,2), 2);
+
+% initialize temperatures
+T_euler(1,1) = Ta; % temperature of water
+T_euler(1,2) = Ta; %temperature of egg/potato
+T_tra(1,1) = Ta; % temperature of water
+T_tra(1,2) = Ta; %temperature of egg/potato
+
+% define ratio of time step and time constant
+h_w = dt/tau_w % this should be small enough, ideally smaller than 0.1
+h_e = dt/tau_e 
+
+% integrate differential equations numerically
+for i=1:imax
+    
+        % Euler's method
+        % water temperature (index 1)
+        T_euler(i+1, 1) = (1 - h_w)*T_euler(i, 1) + h_w*Tw_inf;
+        
+        % % clip maximum water temperature to realistic value
+        % if clip & T(i+1, 1) > Tw_inf_real
+        %     T(i+1, 1) = Tw_inf_real;
+        %     Tw_analyt(i+1) = Tw_inf_real;
+        % end                
+
+        % egg temperature (index 2)
+        T_euler(i+1, 2) = (1 - h_e)*T_euler(i, 2) + h_e*T_euler(i, 1);
+        
+
+        % Heun's or trapezium method
+        % water temperature (index 1)
+        T_tra(i+1, 1) = (1-0.5*h_w)/(1+0.5*h_w)*T_tra(i, 1) + h_w/(1+0.5*h_w)*Tw_inf;
+        
+        % % clip maximum water temperature to realistic value
+        % if clip & T(i+1, 1) > Tw_inf_real
+        %     T(i+1, 1) = Tw_inf_real;
+        %     Tw_analyt(i+1) = Tw_inf_real;
+        % end 
+        
+        T_tra(i+1, 2) = (1-0.5*h_e)/(1+0.5*h_e)*T_tra(i, 2)...
+                  + h_e/(1+0.5*h_e)*(T_tra(i, 1) + T_tra(i+1, 1))/2;
+end
+
+
+% temperature of water, euler
+subplot(3,1,1);
+plot(t, Tw_analyt, '-', t, T_euler, 'o--');
+xlabel('time, t / s')
+ylabel('temperatures, T / °C')
+legend('analyt: water', 'num: water', 'num: potato', 'error','Location','east')
+title('Euler Forward')
+
+% temperature of water, euler, trapezium
+subplot(3,1,2);
+plot(t, Tw_analyt, '-', t, T_tra, 'o--');
+xlabel('time, t / s')
+ylabel('temperatures, T / °C')
+legend('analyt: water', 'num: water', 'num: potato', 'error','Location','east')
+title('Trapezium')
+
+subplot(3,1,3);
+plot(t, Tw_analyt'-T_euler(:,1),'-',t,Tw_analyt'-T_tra(:,1),'o');
+xlabel('time, t / s')
+legend('Euler', 'Trapezium')
+ylabel('temperatures, T / °C')
+title('error')
+
+
+
+
+
+
+
+
