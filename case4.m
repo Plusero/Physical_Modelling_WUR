c79ea570d102bdd297b32035dd3c42f5064242c5
Garin <61001058+Plusero@users.noreply.github.com>
2024-01-15 15:23:56 +0100

    1.15 update afternoon

---
+++ case4.m
@@ -0,0 +1,25 @@
+%properties of aluminium
+lambda_al=237; %W/m/K
+rho_al=2700; %kg/m3
+c_p_al=880; %J/kg/K
+c_al=lambda_al/rho_al/c_p_al;
+
+%properties of copper
+lambda_co=390;
+rho_co=8960;
+c_p_co=390;
+c_co=lambda_co/rho_co/c_p_co;
+
+% properties  of silver
+lambda_si=79;
+rho_si=7870;
+c_p_si=460;
+c_si=lambda_si/rho_si/c_p_si;
+
+% property of steel
+lambda_st=27;
+rho_st=7800;
+c_p_st=460;
+c_st=lambda_st/rho_st/c_p_st;
+
+
