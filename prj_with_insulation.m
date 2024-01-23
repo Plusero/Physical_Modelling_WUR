260ae1d3e0ce0b65a0e21b8e9385e82568255805
Garin <61001058+Plusero@users.noreply.github.com>
2024-01-23 17:29:12 +0100

    1.23 afternoon update

--- prj_with_insulation.m
+++ prj_with_insulation.m
@@ -1,56 +1,73 @@
-clear;
 clc;
 clear all;
-%lambda of cable material
+close all;
+%% Calculation of cable metal part
+%thermal conductivity of cable material, W/m/K
 lambda_c=390;
-%density of cable material
+%density of cable material, kg/m3
 rho_c=8960;
-%thermal capacity of cable material
+%thermal capacity of cable material, J/kg/K
 cp_c=390;
-%current in cable
-I=380;
-% surface area m2
-A=0.01;
-%cable volume resisitvitiy to Ohm/m3
-resistivity_c=1.77*10^-8;
-resistivity_c_m3=resistivity_c/A/A;
-
+%current in cable, A
+I=1000;
+%copper resisitvitiy, Ohm*m
+resistivity_c=1.68*10^-8;
+%radius of cable cross section, m
+r=0.13/2;
+%cross sectional area of cable, m2
+A=pi*r^2;
+%Resistance per volume, Ohm/m3
+resistivity_vol=resistivity_c/(A^2);
 %constant c for cable
-c_c=lambda_c/(rho_c*cp_c);
+% c_c=lambda_c/(rho_c*cp_c);
+c_c=lambda_c;
 %constant f for cable
-f_c=I^2*resistivity_c_m3/(cp_c*rho_c);
+% f_c=I^2*resistivity_vol/(cp_c*rho_c);
+f_c=I^2*resistivity_vol;
 
-%outside temperature 
-Ta=20 ;
-%radius of cable cross section
-r=0.01;
-%lambda of air
+%% Calculation of cable insulation
+%thermal conductivity, W/m/K
+lambda_i=0.27;
+%heat capacity insulation material, J/kg/K
+cp_i= 2000;
+%density of insulation material, kg/m3
+rho_i=920;
+%C factor for insulation material
+% c_i=lambda_i/(rho_i*cp_i);
+c_i=lambda_i;
+
+%% Heat Transfer Coeffient for boundary
+%lambda of air, W/m/K
 lambda_a=0.59;
-%dynamic viscosity of air
+%dynamic viscosity of air, N s m-2
 mu_a=0.6513*10^-3;
-%thermal capacity air
+%thermal capacity air,  J/kg/K
 cp_a=4200;
-%density air
+%density air, kg/m3
 rho_a=1.3;
 %velocity of air, m/s
-vel_a=1;
+vel_a=4; % in July
+
+%thickness of insulation, m
+th=0.13;
 %Pr number
 pr_a= (mu_a*cp_a)/lambda_a;
 %Reynolds number
 re_a=(rho_a*vel_a*2*r)/mu_a;
 %nusselt number laminar component
 nu_lam=0.664*re_a^0.5*pr_a^(1/3);
-global alpha_ca
 alpha_ca=nu_lam/((2*r)/lambda_a);
+global alpha
+alpha=alpha_ca;
 
 C1 = [1
     0
     0
-    1];
+    r];
 C2 = [1
     0
     0
-    2];
+    r+th];
 geom = [C1 C2];
 ns = char('C1','C2');
 ns = ns';
@@ -67,17 +84,17 @@ model = createpde
 geometryFromEdges(model,g);
 figure
 pdegplot(model,"EdgeLabels","on","FaceLabels","on")
-xlim([-1.1 1.1])
+xlim([-2*r 2*r])
 axis equal
 
-msh=generateMesh(model,"Hmax",0.1);
+msh=generateMesh(model,"Hmax",0.01);
 figure
 pdemesh(msh)
 axis equal
 
 %% apply Neumann Boundary condition
 applyBoundaryCondition(model,"neumann", ...
-                             "Edge",[4:8], ...
+                             "Edge",[5:8], ...
                              "g",@bcfuncN);
 
 %sets initial conditions                         
@@ -85,12 +102,12 @@ setInitialConditions(model,25);
 
 % Specify Coefficient of PDE
 specifyCoefficients(model,"m",0,"d",0,"c",c_c,"a",0,"f",f_c,"Face",1);
-specifyCoefficients(model,"m",0,"d",0,"c",c_c,"a",0,"f",0,"Face",2);
+specifyCoefficients(model,"m",0,"d",0,"c",c_i,"a",0,"f",0,"Face",2);
 % d=0, for steady state; d=1, for transient
 
 theta = linspace(0, 2*pi, 100);  % Generate 100 points around the circle
-x = cos(theta);
-y = sin(theta);
+x = r*cos(theta);
+y = r*sin(theta);
 
 results = solvepde(model);
 u = results.NodalSolution;
@@ -101,14 +118,12 @@ plot(x, y);
 axis equal; 
 hold on
 
-
 function bc = bcfuncN(location,state);
-    global alpha_ca;
-    alpha=alpha_ca;
+    global alpha;
     %Convection term
     Ta=20;
     bc = alpha*(state.u-Ta);
-    %scatter(location.x,location.y,"filled","red");
+    % scatter(location.x/2,location.y/2,"filled","red");
     hold on 
 end
 
